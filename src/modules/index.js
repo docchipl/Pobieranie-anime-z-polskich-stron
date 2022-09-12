8458f3aa8319ca22e24113ffe95708301d007a55
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-09-12 21:28:33 +0200

    update index

--- src/modules/index.js
+++
@@ -1,7 +0,0 @@
-export { default as FrixySubs } from "./frixysubs.js";
-export { default as OkamiSubs } from "./okamisubs.js";
-export { default as MioroSubs } from "./miorosubs.js";
-export { default as MaouSubs } from "./maousubs.js";
-export { default as DayidSub } from "./dayidsub.js";
-export { default as NanaSubs } from "./nanasubs.js";
-export { default as Fumetsu } from "./fumetsu.js";
