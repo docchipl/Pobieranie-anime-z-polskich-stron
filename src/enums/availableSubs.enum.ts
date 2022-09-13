015673ece1382c80bc8e4d2737087c85ff47da72
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-09-13 19:51:41 +0200

    refactoring index and adding available subs enum

---
+++ src/enums/availableSubs.enum.ts
@@ -0,0 +1,9 @@
+export enum AvailableSubs {
+  OkamiSubs = 'OkamiSubs',
+  MioroSubs = 'MioroSubs',
+  MaouSubs = 'MaouSubs',
+  DayidSub = 'DayidSub',
+  NanaSubs = 'NanaSubs',
+  FumetsuSubs = 'FumetsuSubs',
+  FrixySubs = 'FrixySubs',
+}
--- src/interfaces/response.ts
+++ src/interfaces/response.ts
@@ -1,9 +1,11 @@
 export interface AnimeSubsApiResponse {
   status: number;
   message: string;
-  episode_url?: {
-    player: string | null;
-    url: string | null;
-  }[];
+  episode_url?:
+    | {
+        player: string;
+        url: string;
+      }[]
+    | null;
   episode_next_url?: string | null;
 }
