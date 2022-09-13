8460e3ff8274391be3e42b92c472f7fac32d09fd
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-09-13 21:46:14 +0200

    okamisubs

--- src/modules/okamisubs.ts
+++ src/modules/okamisubs.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as jsdom from 'jsdom';
+import { AxiosClient } from './axiosClient';
 
 const { JSDOM } = jsdom;
 const virtualConsole = new jsdom.VirtualConsole();
@@ -7,44 +8,41 @@ virtualConsole.on('error', () => {
   // No-op to skip console errors.
 });
 
-function OkamiSubs(anime: string, episode: string) {
-  const request = axios
-    .get(`https://okami-subs.pl/anime/${anime}/odcinek/${episode}`)
-    .then(function (response) {
-      const dom = new JSDOM(response.data, { virtualConsole });
-      const episode_url_cleaning = JSON.parse(
-        '[' + dom.window.document.getElementsByTagName('script')[4].textContent!.match(/\[(.+)\]/)![1] + ']',
-      );
-      let episode_next_url;
+const OkamiSubs = async (anime: string, episode: string) => {
+  try {
+    const { data } = await new AxiosClient(`https://okami-subs.pl/anime/${anime}/odcinek/${episode}`).get<string>();
+    const dom = new JSDOM(data, { virtualConsole });
+    const episode_cleaning = JSON.parse(
+      '[' + dom.window.document.getElementsByTagName('script')[4].textContent!.match(/\[(.+)\]/)![1] + ']',
+    );
+    let episode_next_url;
 
-      if (dom.window.document.querySelector('.col-md-4.col-xs-12 .button.pull-right')) {
-        episode_next_url = (dom.window.document.querySelector(
-          '.col-md-4.col-xs-12 .button.pull-right',
-        ) as HTMLAnchorElement)!.href;
-      } else {
-        episode_next_url = null;
-      }
+    if (dom.window.document.querySelector('.col-md-4.col-xs-12 .button.pull-right')) {
+      episode_next_url = (dom.window.document.querySelector(
+        '.col-md-4.col-xs-12 .button.pull-right',
+      ) as HTMLAnchorElement)!.href;
+    } else {
+      episode_next_url = null;
+    }
 
+    const episode_url = episode_cleaning.map((x: any) => {
       return {
-        status: 200,
-        message: 'Success',
-        episode_url: episode_url_cleaning.map(function (x: any) {
-          return {
-            player: x.player,
-            url: x.url,
-          };
-        }),
-        episode_next_url: episode_next_url,
-      };
-    })
-    .catch((err) => {
-      //console.log(err)
-      return {
-        status: 500,
-        message: 'Something went wrong!',
+        player: x.player,
+        url: x.url,
       };
     });
 
-  return request;
-}
+    return {
+      status: 200,
+      message: 'Success',
+      episode_url,
+      episode_next_url: episode_next_url,
+    };
+  } catch {
+    return {
+      status: 500,
+      message: 'Something went wrong!',
+    };
+  }
+};
 export default OkamiSubs;
