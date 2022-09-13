05def25e91ad8a82ca056b1e25f3bb1c5b964808
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-09-13 21:39:01 +0200

    miorosubs

--- src/modules/miorosubs.ts
+++ src/modules/miorosubs.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import * as jsdom from 'jsdom';
-import { AnimeSubsApiResponse } from '../interfaces';
+import { AnimeSubsApiResponse, AnimeSubsEpisode } from '../interfaces';
+import { AxiosClient } from './axiosClient';
 
 const { JSDOM } = jsdom;
 const virtualConsole = new jsdom.VirtualConsole();
@@ -8,41 +9,35 @@ virtualConsole.on('error', () => {
   // No-op to skip console errors.
 });
 
-function MioroSubs(anime: string, episode: string): Promise<AnimeSubsApiResponse> {
-  const request = axios
-    .get(`https://miorosubs.7m.pl/${anime}-${episode}`, {
-      headers: {
-        Referer: `https://miorosubs.7m.pl/${anime}-${episode}`,
-        'X-Requested-With': 'XMLHttpRequest',
-      },
-    })
-    .then(function (response) {
-      const dom = new JSDOM(response.data, { virtualConsole });
+const MioroSubs = async (anime: string, episode: string): Promise<AnimeSubsApiResponse> => {
+  try {
+    const { data } = await new AxiosClient(`https://miorosubs.7m.pl/${anime}-${episode}`).get<string>();
+    const dom = new JSDOM(data, { virtualConsole });
 
-      let episode_url_cleaning = [];
-      const items = dom.window.document.querySelector('#mirror')!.querySelectorAll('option');
-      for (var i = 0; i < items.length; ++i) {
-        episode_url_cleaning.push({
-          player: items[i].textContent!.split(' ')[0],
-          url: items[i].value,
-        });
-      }
+    let episode_cleaning: AnimeSubsEpisode[] = [];
+    const items = dom.window.document.querySelector('#mirror')!.querySelectorAll('option');
 
-      return {
-        status: 200,
-        message: 'Success',
-        episode_url: episode_url_cleaning,
-        episode_next_url: `${Number(episode) + 1}`,
-      };
-    })
-    .catch((err) => {
-      //console.log(err)
-      return {
-        status: 500,
-        message: 'Something went wrong!',
-      };
+    items.forEach((item) => {
+      const player = item.textContent!.split(' ')[0];
+      const url = item.value;
+      if (!player || !url) return null;
+      episode_cleaning.push({
+        player,
+        url,
+      });
     });
 
-  return request;
-}
+    return {
+      status: 200,
+      message: 'Success',
+      episode: episode_cleaning,
+      episode_next_url: `${Number(episode) + 1}`,
+    };
+  } catch {
+    return {
+      status: 500,
+      message: 'Something went wrong!',
+    };
+  }
+};
 export default MioroSubs;
